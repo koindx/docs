04015e1889e22a683ae6e0eee525c0652e5c0f3c
kqkqkqkqkqkqkq@pm.me <kqkqkqkqkqkqkq@pm.me>
2022-04-30 18:37:08 +0200

    feat: removed editUrls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/koindx/docs/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/koindx/docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
